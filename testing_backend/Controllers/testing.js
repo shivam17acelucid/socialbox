b0bf3b64cf03d2d3b85df2b4a49b25228f83bf93
shivam17acelucid <shivam.rawat@acelucid.com>
2022-10-26 16:14:24 +0530

    initial

--- testing_backend/Controllers/testing.js
+++ testing_backend/Controllers/testing.js
@@ -2,13 +2,13 @@ const fetch = require('node-fetch');
 const User = require('../models/user');
 const Username = require('../Models/username');
 const ProfileData = require('../Models/profile_data');
-const axios = require('axios')
+const axios = require('axios');
+const { downloadcsv } = require('./downloadcsv');
 const URLENCODED_HEADER = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
     'User-Agent': 'Mozilla / 5.0(iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/ 605.1.15(KHTML, like Gecko) Mobile / 15E148 Instagram 105.0.0.11.118(iPhone11, 8; iOS 12_3_1; en_US; en - US; scale = 2.00; 828x1792; 165586599)',
-    // 'Cookie': 'sessionid=55174935431:x8JvcGYu82tJ6d:28:AYfI627ANegNpG_tFchbkKqtxROQAlXVXdeR9Xi4Sg',
-    'Cookie': 'sessionid=4254722113:8jQkarLtt6Z1wj:27:AYdZDexKTjHFIqPlMKx-vfND1fPhT1vP3FfU8Fii1g',
+    // 'Cookie': 'sessionid='
 }
 const TAG_API_HEADER = {
     'Accept': 'application/json',
@@ -83,32 +83,25 @@ exports.hashtag = (req, res, next) => {
         });
 }
 
-exports.username = (req, res, next) => {
-    let { username } = req.body;
-    const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${username}`;
-    axios.get(url, {
-        method: 'GET',
-        headers: URLENCODED_HEADER,
-        mode: 'cors'
-    }).then((response) => {
-        res.status(200).json({
-            success: true,
-            data: response.data
-        });
-        console.log(response.data)
-    })
-}
-
 exports.topsearch = async (req, res, next) => {
     let { query } = req.body;
     const url = `https://www.instagram.com/web/search/topsearch?query=${query}`;
-    fetch(url, {
+    axios.get(url, {
         method: 'GET',
-        headers: URLENCODED_HEADER,
-        // redirect: 'follow',
-        // follow: 20,
-    }).then((res) => res.json())
-        .then((data) => res.json(data));
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla / 5.0(iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/ 605.1.15(KHTML, like Gecko) Mobile / 15E148 Instagram 105.0.0.11.118(iPhone11, 8; iOS 12_3_1; en_US; en - US; scale = 2.00; 828x1792; 165586599)',
+            'Cookie': 'sessionid=55174935431:oUkeyOkjSTHOai:11:AYcCQRGAQW0_XSTcjaz7XHUgRoXj6weyCzi_8MCzew',
+            // 'Cookie': 'sessionid=4254722113:8jQkarLtt6Z1wj:27:AYdZDexKTjHFIqPlMKx-vfND1fPhT1vP3FfU8Fii1g',   //working
+            // 'Cookie': 'sessionid=1606001920:gScv2hwUIuRb82:21:AYevLdDnNu06Sv5fsj-r-Pm5NYjUL8ZNfHSGrPv3UQ',
+        },
+    }).then((res) => {
+        // res.json()
+        console.log(response)
+    })
+    // .then((data) => res.json(data));
+
 }
 
 exports.userID = (req, res, next) => {
@@ -161,40 +154,109 @@ exports.profile = (req, res, next) => {
             })
             {
                 arr.forEach(item => {
-                    item.follower_count > '2000000' ?
+                    item.follower_count > '10000' ?
                         bigAccount.push(item)
                         : null
                 })
                 console.log(bigAccount)
                 bigAccount.forEach((item) => {
-                const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${item.username}`;
-                axios.get(url, {
-                    method: 'GET',
-                    headers: URLENCODED_HEADER,
-                    mode: 'cors',
-                })
-                    .then((response) => {
-                        // res.status(200).json({
-                        //     success: true,
-                        //     data: response.data
-                        // });
-                        console.log([response.data['data']['user']])
-                         ProfileData.insertMany([response.data['data']['user']])
+                    const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${item.username}`;
+                    axios.get(url, {
+                        method: 'GET',
+                        headers: URLENCODED_HEADER,
+                        mode: 'cors',
+                    })
+                        .then((response) => {
+                            console.log(response.data)
+                            // res.status(200).json({
+                            //     success: true,
+                            //     data: response.data
+                            // });
+                            console.log([response.data['data']['user']])
+                            ProfileData.insertMany([response.data['data']['user']])
                                 .then((result) => {
-                                    res.json({
-                                        success: 'true',
-                                        result
-                                    })
+                                    // res.json({
+                                    //     success: 'true',
+                                    //     result: response.data['data']['user']
+                                    // })
                                 })
                                 .catch((err) => {
                                     console.log(err)
                                 })
-                    })
+                        })
                 })
             }
 
         })
         .catch((err) => {
             console.log(err)
+        })
+}
+
+exports.username = (req, res, next) => {
+    let { username } = req.body;
+    const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${username}`;
+    axios.get(url, {
+        method: 'GET',
+        headers: URLENCODED_HEADER,
+        mode: 'cors'
+    }).then((response) => {
+        res.status(200).json({
+            success: true,
+            data: response.data
+        });
+        console.log(response.data)
+    })
+}
+
+// exports.influencer_list = (req, res, next) => {
+//     let { username, follower_count, category_enum, full_name } = req.body;
+//     // let query = {};
+//     let filter = {};
+//     if (username != null) {
+//         filter.username = username;
+//         // { username: { $regex: username, $options: 'i' } }
+//     }
+//     if (full_name != null) {
+//         filter.full_name = full_name;
+//     }
+//     if (follower_count != null) {
+//         filter.follower_count = follower_count;
+//     }
+//     if (category_enum != null) {
+//         filter.category_enum = category_enum;
+//     }
+//     ProfileData.find(filter)
+//         .then((data) => {
+//             // console.log(data)
+//             res.json({
+//                 success: 'true',
+//                 data
+//             })
+//         })
+// }
+
+exports.influencer_list = (req, res, next) => {
+    let { username, followers, category, name } = req.query;
+    let filter = {};
+    if (username != null) {
+        filter.username = new RegExp(username, 'i');
+    }
+    if (name != null) {
+        filter.full_name = new RegExp(name, 'i');
+    }
+    if (followers != null) {
+        filter = { edge_followed_by: { count: parseInt(followers) } }
+    }
+    if (category != null) {
+        filter.category_enum = new RegExp(category, 'i');
+    }
+    ProfileData.find(filter)
+        .then((data) => {
+            console.log(filter)
+            res.json({
+                success: 'true',
+                data
+            })
         })
 }
--- testing_backend/Routes/index.js
+++ testing_backend/Routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { hashtag, username, topsearch, userID, profile } = require('../Controllers/testing');
+const { hashtag, username, topsearch, userID, profile, influencer_list } = require('../Controllers/testing');
 const { uploadcsv, uploadCsv } = require('../Controllers/uploadcsv')
 const { downloadcsv } = require("../controllers/downloadcsv");
 
@@ -9,6 +9,7 @@ router.get('/searchbyusername', username)
 router.get('/searchtopusers', topsearch)
 router.get('/getUsername', userID)
 router.get('/getprofiledata', profile)
+router.get('/getinfluencerdata', influencer_list)
 router.post("/upload-csv", uploadCsv, uploadcsv);
 router.get("/downloadcsv", downloadcsv);
 module.exports = router;

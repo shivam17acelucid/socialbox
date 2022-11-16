b85999033483bb1b4f39c92a02bd8c00fe78e458
shivam17acelucid <shivam.rawat@acelucid.com>
2022-11-16 15:16:27 +0530

    initial

--- testing/src/Components/BasketInfluencers/index.js
+++ testing/src/Components/BasketInfluencers/index.js
@@ -13,13 +13,20 @@ import TableFooter from '@mui/material/TableFooter';
 import TablePagination from '@mui/material/TablePagination';
 import NFormatter from '../../Common/NumberFormatter/numFormatter';
 import { useNavigate, useParams } from "react-router-dom";
+import { MdAdd } from 'react-icons/md';
 
 function BasketInfluencers() {
 
     const [inputField, setInputField] = useState('');
     const [influencersData, setInfluencersData] = useState([]);
+    const [addToListTableClicked, setAddToListTableClicked] = useState(false);
+    const [listData, setListData] = useState([]);
+    const [newPlanClicked, setNewPlanClicked] = useState(false);
+    const [rowClickedData, setRowClickedData] = useState('');
+
     let navigate = useNavigate();
     let { categoryName } = useParams();
+    const userId = localStorage.getItem('id');
 
     const searchInfluencers = () => {
         navigate(`/influencerslist/${inputField}`);
@@ -34,12 +41,41 @@ function BasketInfluencers() {
             })
     }
 
+    const getListData = () => {
+        const url = `http://localhost:4000/getListData/${userId}`;
+        fetch(url)
+            .then((data) => {
+                data.json()
+                    .then((result) => {
+                        setListData(result)
+                    })
+            })
+    }
+
     const redirectProfile = (data) => {
         navigate(`/profile/${data.username}`)
     }
 
+    const handleAddToListTable = (data) => {
+        setRowClickedData(data.username)
+        const toggle = addToListTableClicked ? false : true;
+        setAddToListTableClicked(toggle);
+    }
+
+    const addInfluencerToList = (data, item) => {
+        const url = `http://localhost:4000/addInfluencersToList/${userId}?list=${item.listName}&username=${data.username}`
+        fetch((url), {
+            method: 'POST',
+        })
+            .then((data) => { data.json() })
+            .then((res) => {
+                console.log(res)
+            })
+    }
+
     useEffect(() => {
         fetchInfluencers();
+        getListData();
     }, []);
 
     return (
@@ -96,6 +132,30 @@ function BasketInfluencers() {
                                                             {NFormatter(data.edge_owner_to_timeline_media['edges'][0].er)}
                                                         </TableCell>
                                                         <TableCell align="center">{data.city_name}</TableCell>
+                                                        <TableCell key={index} >
+                                                            <>
+                                                                <Button id={data.id} onClick={() => { handleAddToListTable(data) }}><span><MdAdd /> To List</span></Button>
+                                                                {addToListTableClicked === true ?
+                                                                    [data].map((item) =>
+                                                                        item.username == rowClickedData ?
+                                                                            <section className="addList_section" id={data.id}>
+                                                                                <div className="addList_option">
+                                                                                    <div onClick={() => setNewPlanClicked(true)} className='section_dropdown_header'>To New List <span><MdAdd /></span></div>
+                                                                                    <div>Recently Created Lists</div>
+                                                                                    {listData.map((item) =>
+                                                                                        <div className="list_options" onClick={() => { addInfluencerToList(data, item) }}>
+                                                                                            {item.listName}
+                                                                                        </div>
+                                                                                    )}
+                                                                                </div>
+                                                                            </section>
+                                                                            :
+                                                                            console.log("galat")
+                                                                    )
+
+                                                                    : null}
+                                                            </>
+                                                        </TableCell>
                                                     </TableRow>
                                                 )
                                             }

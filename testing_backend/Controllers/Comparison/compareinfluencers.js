3e363e5754bcd1da477aa3676d8188c53d32b3e9
shivam17acelucid <shivam.rawat@acelucid.com>
2022-12-01 17:05:19 +0530

    main

--- testing_backend/Controllers/Comparison/compareinfluencers.js
+++ testing_backend/Controllers/Comparison/compareinfluencers.js
@@ -2,87 +2,19 @@ const InfluencersData = require('../../Models/influencer_details');
 const UserInfo = require('../../Models/user_info');
 
 exports.compareInfluencers = (req, res) => {
-    let { influencer1name, influencer2name, influencer3name, influencer4name } = req.query;
-    let errors = [];
-    let filter = [];
-
-    if (influencer1name && !influencer2name && !influencer3name && !influencer4name) {
-        errors.push("Minimum 2 Influencers Required")
-    }
-    if (influencer2name && !influencer1name && !influencer3name && !influencer4name) {
-        errors.push("Minimum 2 Influencers Required")
-    }
-    if (influencer3name && !influencer2name && !influencer1name && !influencer4name) {
-        errors.push("Minimum 2 Influencers Required")
-    }
-
-    if (errors.length > 0) {
-        return res.status(422).json({ errors: errors });
-    }
-
-    if (influencer1name && influencer2name) {
-        filter = [
-            { username: influencer1name },
-            { username: influencer2name },
-        ]
-    }
-
-    if (influencer1name && influencer3name) {
-        filter = [
-            { username: influencer1name },
-            { username: influencer3name },
-        ]
-    }
-
-    if (influencer2name && influencer3name) {
-        filter = [
-            { username: influencer2name },
-            { username: influencer3name },
-        ]
-    }
-
-    if (influencer1name && influencer2name && influencer3name) {
-        filter = [
-            { username: influencer1name },
-            { username: influencer2name },
-            { username: influencer3name },
-        ]
-    }
-
-    if (influencer1name && influencer4name) {
-        filter = [
-            { username: influencer1name },
-            { username: influencer3name },
-        ]
-    }
-    
-    if (influencer2name && influencer4name) {
-        filter = [
-            { username: influencer1name },
-            { username: influencer3name },
-        ]
-    }
-
-    if (influencer3name && influencer4name) {
-        filter = [
-            { username: influencer1name },
-            { username: influencer3name },
-        ]
-    }
-
-    if (influencer1name && influencer2name && influencer3name && influencer4name) {
-        filter = [
-            { username: influencer1name },
-            { username: influencer2name },
-            { username: influencer3name },
-            { username: influencer4name },
-        ]
-    }
-
-    // InfluencersData.find({ $and: [filter] })
-    InfluencersData.find({ $or: filter })
+    let { influencers } = req.body;
+    let array = [];
+    InfluencersData.find()
         .then((data) => {
-            console.log(filter)
-            res.json(data)
+            influencers.forEach((item) => {
+                data.filter((el) => {
+                    if (el.username === item) {
+                        console.log(item)
+                        array.push(el)
+                    }
+                })
+            })
+            res.json(array)
         })
+
 }  

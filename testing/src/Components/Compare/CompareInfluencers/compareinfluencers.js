a030cc1a6438fec0f2a709b5fdca761fd2d83e3b
shivam17acelucid <shivam.rawat@acelucid.com>
2022-12-15 12:34:42 +0530

    main

--- testing/src/Components/Compare/CompareInfluencers/compareinfluencers.js
+++ testing/src/Components/Compare/CompareInfluencers/compareinfluencers.js
@@ -8,6 +8,7 @@ import { useParams, useNavigate, Navigate } from "react-router-dom";
 import { HiOutlineUser } from 'react-icons/hi';
 import { FcLike, FcComments, FcBinoculars, FcRating } from 'react-icons/fc';
 import NFormatter from '../../../Common/NumberFormatter/numFormatter';
+import { AiOutlineClose } from 'react-icons/ai';
 
 function CompareInfluencers() {
 
@@ -16,8 +17,13 @@ function CompareInfluencers() {
     const [influencer3Name, setInfluencer3Name] = useState('');
     const [influencersArray, setInfluencersArray] = useState([]);
     const [comparedInfluencersData, setComparedInfluencersData] = useState([]);
+    const [paramsChanged, setParamsChanged] = useState(false);
+    const [addToListTableClicked, setAddToListTableClicked] = useState(false);
+    const [listData, setListData] = useState([]);
+    const [resultClickedData, setResultClickedData] = useState('');
     const params = useParams();
     const navigate = useNavigate();
+    const userId = localStorage.getItem('id');
 
     // const handleCompareInfluencers = (influencer1Name, influencer2Name, influencer3Name) => {
     //     setComparedInfluencersData([]);
@@ -47,26 +53,66 @@ function CompareInfluencers() {
         //     })
     }
 
+    const handleAddInfluencer = () => {
+
+    }
+
+    const handleAddToListTable = (e) => {
+        setResultClickedData(e.username)
+        const toggle = addToListTableClicked ? false : true;
+        setAddToListTableClicked(toggle);
+    }
+
+    const getListData = () => {
+        const url = `http://localhost:4000/getListData/${userId}`;
+        fetch(url)
+            .then((data) => {
+                data.json()
+                    .then((result) => {
+                        setListData(result)
+                    })
+            })
+    }
+
+    const addInfluencerToList = (element, elm) => {
+        const url = `http://localhost:4000/addInfluencersToList/${userId}?list=${elm.listName}&username=${element.username}`
+        fetch((url), {
+            method: 'POST',
+        })
+            .then((data) => { data.json() })
+            .then((res) => {
+                console.log(res)
+            })
+        setAddToListTableClicked(false)
+    }
+
     const redirectToProfile = (item) => {
         navigate(`/profile/${item.username}`)
     }
 
     const handleCompareInfluencersByParams = () => {
-        let query = params.influencers.substring(1);
-        const url = `http://localhost:4000/compareInfluencers?${query}`;
-        fetch(url)
-            .then((res) => {
-                res.json()
-                    .then((data) => {
-                        setComparedInfluencersData(data)
-                    })
-            })
+        if (JSON.stringify(params) !== '{}') {
+            let query = params.influencers.substring(1);
+            const url = `http://localhost:4000/compareInfluencers?${query}`;
+            fetch(url)
+                .then((res) => {
+                    res.json()
+                        .then((data) => {
+                            setComparedInfluencersData(data)
+                        })
+                })
+        }
+        else {
+            console.log("hey")
+            setComparedInfluencersData([])
+        }
 
     }
 
     useEffect(() => {
         handleCompareInfluencersByParams();
-    }, [])
+        getListData();
+    }, [params])
 
     return (
         <div className='compare_influencers_container'>
@@ -119,7 +165,30 @@ function CompareInfluencers() {
                                     <div className='profile_comment'><FcRating /><span>{NFormatter(item.edge_owner_to_timeline_media.edges[0].er)}</span></div>
                                 </div>
                                 <div className='list_remove_pane'>
-                                    <div>+Add to my List</div>
+                                    <div onClick={() => { handleAddToListTable(item) }}>+Add to my List</div>
+                                    {addToListTableClicked === true ?
+                                        item.username == resultClickedData ?
+                                            [item].map((elm) =>
+                                                <section className="addList_section" id={item.id}>
+                                                    <div className="addList_option">
+                                                        <div style={{ display: 'flex', justifyContent: 'flex-end' }} onClick={handleAddToListTable}><AiOutlineClose /></div>
+                                                        <div className="section_list_title">
+                                                            Select the list to which you want to add the
+                                                            influencer.
+                                                        </div>
+                                                        <div style={{ overflowY: 'scroll', height: '20vh', padding: '12px' }}>
+                                                            {listData.map((element) =>
+                                                                <div className="list_options" onClick={() => { addInfluencerToList(elm, element) }}>
+                                                                    {element.listName}
+                                                                </div>
+                                                            )}
+                                                        </div>
+                                                    </div>
+                                                </section>
+                                            )
+
+                                            : null
+                                        : null}
                                     <div>Remove</div>
                                 </div>
                                 <div className='recent_posts'>
@@ -152,6 +221,10 @@ function CompareInfluencers() {
                             </div>
                         )
                     }
+                    <div className='add_influencers'>
+                        <div className='add_btn' onClick={handleAddInfluencer}>+</div>
+                        <div className='add_inf' onClick={handleAddInfluencer}>Add Influencer</div>
+                    </div>
                 </div>
             </div>
         </div >

0502cacdaf6a630da23a7eb207ca41bf04b077d5
shivam17acelucid <shivam.rawat@acelucid.com>
2022-12-28 13:24:48 +0530

    main

--- testing_backend/Controllers/enquiry.js
+++ testing_backend/Controllers/enquiry.js
@@ -1,7 +1,17 @@
 const Enquiry = require('../Models/enquiry');
+const emailRegxp =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
 
 exports.submitQuery = (req, res, next) => {
     let { firstName, lastName, phone, email, message } = req.body;
+    let errors = [];
+    if (!emailRegxp.test(email)) {
+        errors.push("invalid email");
+    }
+    if (errors.length > 0) {
+        return res.status(422).json({ errors: errors });
+    }
     const enquiry = new Enquiry({
         firstName: firstName,
         lastName: lastName,

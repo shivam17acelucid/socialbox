96015f3c1cd1df8fa8a434361f397d8e7c24c6e4
shivam17acelucid <shivam.rawat@acelucid.com>
2022-12-29 11:41:09 +0530

    main

--- testing_backend/Controllers/Calculator/index.js
+++ testing_backend/Controllers/Calculator/index.js
@@ -16,27 +16,58 @@ exports.setBasicFieldsForReel = (req, res) => {
     let midTierFollowers = {};
     let macroFollowers = {};
     let megaFollowers = {};
+    let reel = {};
 
-    if (minFollowersRange > 1000 && maxFollowersRange < 10000) {
-        nanoFollowers.budgetperFollowersRange = budgetperFollowersRange;
-        nanoFollowers.creatorsCount = creatorsCount;
-        nanoFollowers.reach = reach;
-        nanoFollowers.multipleForBudgetReach = multipleForBudgetReach;
-        nanoFollowers.multipleForBudgetLikesComment = multipleForBudgetLikesComment;
-        nanoFollowers.LikesComment = LikesComment;
-    }
+    nanoFollowers.budgetperFollowersRange = budgetperFollowersRange;
+    nanoFollowers.creatorsCount = creatorsCount;
+    nanoFollowers.multipleForBudgetReach = multipleForBudgetReach;
+    nanoFollowers.multipleForBudgetLikesComment = multipleForBudgetLikesComment;
+    nanoFollowers.reach = budgetperFollowersRange * multipleForBudgetReach;
+    nanoFollowers.LikesComment = multipleForBudgetLikesComment * budgetperFollowersRange;
 
-    if (minFollowersRange > 10000 && maxFollowersRange < 50000) {
-        microFollowers.budgetperFollowersRange = budgetperFollowersRange;
-        microFollowers.creatorsCount = creatorsCount;
-        microFollowers.reach = reach;
-        microFollowers.multipleForBudgetReach = multipleForBudgetReach;
-        microFollowers.multipleForBudgetLikesComment = multipleForBudgetLikesComment;
-        microFollowers.LikesComment = LikesComment;
-    }
+    budgetperFollowersRange = 3.5 * budgetperFollowersRange;
+    multipleForBudgetReach = multipleForBudgetReach - multipleForBudgetReach * (10 / 100)
+    multipleForBudgetLikesComment = multipleForBudgetLikesComment - multipleForBudgetLikesComment * (10 / 100);
 
-    Calculator.find()
-        .then((data) => {
-            res.json(data)
-        })
+    microFollowers.budgetperFollowersRange = budgetperFollowersRange;
+    microFollowers.creatorsCount = creatorsCount;
+    microFollowers.multipleForBudgetReach = multipleForBudgetReach;
+    microFollowers.multipleForBudgetLikesComment = multipleForBudgetLikesComment;
+    microFollowers.LikesComment = budgetperFollowersRange * multipleForBudgetLikesComment;
+    microFollowers.reach = budgetperFollowersRange * multipleForBudgetReach;
+
+    budgetperFollowersRange = 3.5 * microFollowers.budgetperFollowersRange;
+    multipleForBudgetReach = microFollowers.multipleForBudgetReach - multipleForBudgetReach * (10 / 100)
+    multipleForBudgetLikesComment = microFollowers.multipleForBudgetLikesComment - multipleForBudgetLikesComment * (10 / 100);
+
+    midTierFollowers.budgetperFollowersRange = budgetperFollowersRange;
+    midTierFollowers.creatorsCount = creatorsCount;
+    midTierFollowers.multipleForBudgetReach = multipleForBudgetReach;
+    midTierFollowers.multipleForBudgetLikesComment = multipleForBudgetLikesComment;
+    midTierFollowers.LikesComment = multipleForBudgetLikesComment * budgetperFollowersRange;
+    midTierFollowers.reach = multipleForBudgetReach * budgetperFollowersRange;
+
+    budgetperFollowersRange = 3.5 * midTierFollowers.budgetperFollowersRange;
+    multipleForBudgetReach = midTierFollowers.multipleForBudgetReach - multipleForBudgetReach * (10 / 100)
+    multipleForBudgetLikesComment = midTierFollowers.multipleForBudgetLikesComment - multipleForBudgetLikesComment * (10 / 100);
+
+    macroFollowers.budgetperFollowersRange = budgetperFollowersRange;
+    macroFollowers.creatorsCount = creatorsCount;
+    macroFollowers.multipleForBudgetReach = multipleForBudgetReach;
+    macroFollowers.multipleForBudgetLikesComment = multipleForBudgetLikesComment
+    macroFollowers.LikesComment = multipleForBudgetLikesComment * budgetperFollowersRange;
+    macroFollowers.reach = multipleForBudgetReach * budgetperFollowersRange;
+
+    budgetperFollowersRange = 3.5 * macroFollowers.budgetperFollowersRange;
+    multipleForBudgetReach = macroFollowers.multipleForBudgetReach - multipleForBudgetReach * (10 / 100)
+    multipleForBudgetLikesComment = macroFollowers.multipleForBudgetLikesComment - multipleForBudgetLikesComment * (10 / 100);
+
+    megaFollowers.budgetperFollowersRange = budgetperFollowersRange;
+    megaFollowers.creatorsCount = creatorsCount;
+    megaFollowers.multipleForBudgetReach = multipleForBudgetReach;
+    megaFollowers.multipleForBudgetLikesComment = multipleForBudgetLikesComment;
+    megaFollowers.LikesComment = budgetperFollowersRange * multipleForBudgetLikesComment;
+    megaFollowers.reach = multipleForBudgetReach * budgetperFollowersRange;
+
+    res.json({ nanoFollowers, microFollowers, midTierFollowers, macroFollowers, megaFollowers });
 }
--- testing_backend/Routes/index.js
+++ testing_backend/Routes/index.js
@@ -10,6 +10,7 @@ const { compareCategorizedBaskets, compareUsersLists } = require('../Controllers
 const { setCalculateCostForReel, setCalculateCostForPosts, setCalculateCostForStories, setCalculateCostForIgtv, setInfluencerCost, setCalculateCostForSwipeupStories, setCalculateCostForVideos } = require('../Controllers/CostCalculator/index');
 const rolehandler = require("../controllers/rolehandler");
 const { submitQuery } = require('../Controllers/enquiry');
+const { setBasicFieldsForReel } = require('../Controllers/Calculator/index');
 
 
 router.post("/signup", signup);
@@ -56,4 +57,5 @@ router.get('/erfilteredBasketData', erfilteredBasketData);
 router.post("/upload-csv", uploadCsv, uploadcsv);
 router.get("/downloadcsv", downloadcsv);
 router.post('/submitQuery', submitQuery);
+router.post('/setBasicFieldsForReel', setBasicFieldsForReel)
 module.exports = router;

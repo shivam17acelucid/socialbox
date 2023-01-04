fdf72a2bce3d7e8a0cd0e485334057634df738d9
shivam17acelucid <shivam.rawat@acelucid.com>
2023-01-04 17:49:20 +0530

    cssTesting

--- testing/src/Components/List Page/listcomponent.js
+++ testing/src/Components/List Page/listcomponent.js
@@ -818,38 +818,44 @@ function Lists() {
                                     {
                                         sortingBaskets === true ?
                                             sortedBaskets.map((item) =>
-                                                <div className='influencers_basket_box col-lg-6 col-md-6 col-sm-12 col-xs-12 col-12'>
-                                                    <div className='influencers_image'>
-                                                        <img src={`http://localhost:4000/uploads/${item.image}`} className="influencers_image" />
-                                                    </div>
-                                                    <div className='basket_right_pane'>
-                                                        <div className='influencers_category_header'>
-                                                            Top {item.basketInfluencersCount} {item.categoryName} Influencers Bundle
+                                                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-12">
+                                                    <div className='influencers_basket_box row'>
+                                                        <div className='influencers_image col-lg-5 col-md-5 col-sm-5 col-xs-5 col-5'>
+                                                            <img src={`http://localhost:4000/uploads/${item.image}`} className="influencers_image" />
                                                         </div>
-                                                        <div className='basket_p'>
-                                                            Boost your marketing campaigns with best travel influencers bundle covering top 20 influencers
-                                                        </div>
-                                                        <div className='influencers_footer_btn'>
-                                                            <Button onClick={() => handleRedirectToBasket(item)}><span style={{ margin: 0 }}>View</span></Button>
+                                                        <div className='basket_right_pane col-lg-7 col-md-7 col-sm-7 col-xs-7 col-7'>
+                                                            <div className='influencers_category_header'>
+                                                                Top {item.basketInfluencersCount} {item.categoryName} Influencers Bundle
+                                                            </div>
+                                                            <div className='basket_p'>
+                                                                Boost your marketing campaigns with best travel influencers bundle covering top 20 influencers
+                                                            </div>
+                                                            <div className='influencers_footer_btn'>
+                                                                <Button onClick={() => handleRedirectToBasket(item)}><span style={{ margin: 0 }}>View</span></Button>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             )
                                             :
                                             basketData.map((item) =>
-                                                <div className='influencers_basket_box'>
-                                                    <div className='influencers_image'>
-                                                        <img src={`http://localhost:4000/uploads/${item.image}`} className="influencers_image" />
-                                                    </div>
-                                                    <div className='basket_right_pane'>
-                                                        <div className='influencers_category_header'>
-                                                            Top {item.basketInfluencersCount} {item.categoryName} Influencers Bundle
+                                                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-12">
+                                                    <div className='influencers_basket_box row'>
+                                                        <div className='col-lg-5 col-md-5 col-sm-5 col-xs-5 col-5 pl-0 pr-0'>
+                                                            <div className='influencers_image' >
+                                                                <img src={`http://localhost:4000/uploads/${item.image}`} className="influencers_image" />
+                                                            </div>
                                                         </div>
-                                                        <div className='basket_p'>
-                                                            Boost your marketing campaigns with best travel influencers bundle covering top 20 influencers
-                                                        </div>
-                                                        <div className='influencers_footer_btn'>
-                                                            <Button onClick={() => handleRedirectToBasket(item)}><span style={{ margin: 0 }}>View</span></Button>
+                                                        <div className='basket_right_pane col-lg-7 col-md-7 col-sm-7 col-xs-7 col-7'>
+                                                            <div className='influencers_category_header'>
+                                                                Top {item.basketInfluencersCount} {item.categoryName} Influencers Bundle
+                                                            </div>
+                                                            <div className='basket_p'>
+                                                                Boost your marketing campaigns with ...
+                                                            </div>
+                                                            <div className='influencers_footer_btn'>
+                                                                <Button onClick={() => handleRedirectToBasket(item)}><span style={{ margin: 0 }}>View</span></Button>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>

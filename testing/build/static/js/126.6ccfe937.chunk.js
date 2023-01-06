91426c0afab2fd65e80a26d6ba1231c3a4747e20
shivam17acelucid <shivam.rawat@acelucid.com>
2023-01-06 15:46:47 +0530

    cssTesting

---
+++ testing/build/static/js/126.6ccfe937.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktesting=self.webpackChunktesting||[]).push([[126],{9126:function(t,a,n){n.d(a,{SVs:function(){return h}});var e=n(9983);function h(t){return(0,e.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M2 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"}}]})(t)}}}]);
+//# sourceMappingURL=126.6ccfe937.chunk.js.map

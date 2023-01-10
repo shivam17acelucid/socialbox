3fc695012d2c36afa39572d8b7fed55af5fe4618
shivam17acelucid <shivam.rawat@acelucid.com>
2023-01-10 18:07:29 +0530

    main

--- testing_backend/Controllers/Comparison/compareinfluencers.js
+++ testing_backend/Controllers/Comparison/compareinfluencers.js
@@ -2,18 +2,27 @@ const InfluencersData = require('../../Models/influencer_details');
 const UserInfo = require('../../Models/user_info');
 
 exports.compareInfluencers = (req, res) => {
+    // let { influencers } = req.query;
+    // let array = [];
+    // InfluencersData.find()
+    //     .then((data) => {
+    //         influencers.forEach((item) => {
+    //             data.filter((el) => {
+    //                 if (el.username === item) {
+    //                     array.push(el)
+    //                 }
+    //             })
+    //         })
+    //         res.json(array)
+    //     })
     let { influencers } = req.query;
     let array = [];
-    InfluencersData.find()
+    let filter = [];
+    influencers.forEach((item) => {
+        filter.push({ username: item })
+    })
+    InfluencersData.find({ $or: filter })
         .then((data) => {
-            influencers.forEach((item) => {
-                data.filter((el) => {
-                    if (el.username === item) {
-                        array.push(el)
-                    }
-                })
-            })
-            res.json(array)
+            res.json(data)
         })
-
 }  

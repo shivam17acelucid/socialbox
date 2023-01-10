ef1f7c9625d8731c399d0b64116a1fdc17f11877
shivam17acelucid <shivam.rawat@acelucid.com>
2023-01-10 16:51:14 +0530

    main

--- testing_backend/Controllers/Sorting/index.js
+++ testing_backend/Controllers/Sorting/index.js
@@ -8,7 +8,7 @@ exports.filterUsers = (req, res) => {
         filter = { $or: [{ full_name: { $regex: username, $options: 'i' } }, { username: { $regex: username, $options: 'i' } }, { category_enum: { $regex: username, $options: 'i' } }] }
     }
     InfluencersData.find(filter)
-        .select({ username: 1 })
+        .select({ username: 1, _id: 0 })
         .then((data) => {
             // data.forEach((item) => {
             //     usernameArray.push(item.username)

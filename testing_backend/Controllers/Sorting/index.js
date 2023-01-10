d20553ded51f508ffe6b978857d0a6febf63d21d
shivam17acelucid <shivam.rawat@acelucid.com>
2023-01-10 16:42:37 +0530

    main

--- testing_backend/Controllers/Sorting/index.js
+++ testing_backend/Controllers/Sorting/index.js
@@ -3,11 +3,15 @@ const InfluencersData = require('../../Models/influencer_details');
 exports.filterUsers = (req, res) => {
     let { username } = req.body;
     let filter = {};
+    let usernameArray = [];
     if (username != null) {
         filter = { $or: [{ full_name: { $regex: username, $options: 'i' } }, { username: { $regex: username, $options: 'i' } }, { category_enum: { $regex: username, $options: 'i' } }] }
     }
     InfluencersData.find(filter)
         .then((data) => {
-            res.json(data)
+            data.forEach((item) => {
+                usernameArray.push(item.username)
+            })
+            res.json(usernameArray)
         })
 }

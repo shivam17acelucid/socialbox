9e3ee84a169aa0fdb8c1372b3d4802a587d6ca7c
shivam17acelucid <shivam.rawat@acelucid.com>
2023-01-12 13:30:47 +0530

    main

--- testing_backend/Controllers/testing.js
+++ testing_backend/Controllers/testing.js
@@ -633,7 +633,7 @@ exports.getFilteredResults = (req, res) => {
     let flag = [];
 
     InfluencersData.find(search)
-        .select({ username: 1, _id: 0, edge_followed_by: 1, 'edge_owner_to_timeline_media.edges.avg_likes': 1, 'edge_owner_to_timeline_media.edges.avg_comment': 1, 'edge_owner_to_timeline_media.edges.er': 1, 'edge_felix_video_timeline.edges.averageReelView': 1, 'edge_felix_video_timeline.edges.totalReelView': 1, city_name: 1, category_enum: 1, costFactorPosts: 1, costFactorReel: 1, costFactorStories: 1, costFactorVideo: 1, costFactorIgtv: 1, costFactorSwipeUp: 1 })
+        .select({ username: 1, _id: 0, edge_followed_by: 1, 'edge_owner_to_timeline_media.edges.avg_likes': 1, 'edge_owner_to_timeline_media.edges.avg_comment': 1, 'edge_owner_to_timeline_media.edges.er': 1, 'edge_felix_video_timeline.edges.averageReelView': 1, 'edge_felix_video_timeline.edges.totalReelView': 1, city_name: 1, category_enum: 1, costFactorPosts: 1, costFactorReel: 1, costFactorStories: 1, costFactorVideo: 1, costFactorIgtv: 1, costFactorSwipeUp: 1, full_name: 1, profile_pic_url_hd: 1 })
         .then((data) => {
             flag.push(data)
             if (flag[0].length > 0) {

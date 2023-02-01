4df6517e2cdfed68c102432693b89eae9dceddfb
shivam17acelucid <shivam.rawat@acelucid.com>
2023-02-01 18:15:21 +0530

    fetch apis

--- testing_backend/Controllers/UploadCreatorCsv/index.js
+++ testing_backend/Controllers/UploadCreatorCsv/index.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 
 const URLENCODED_HEADER = {
     'Accept': 'application/json',
-    // 'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
     'User-Agent': 'Mozilla / 5.0(iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/ 605.1.15(KHTML, like Gecko) Mobile / 15E148 Instagram 105.0.0.11.118(iPhone11, 8; iOS 12_3_1; en_US; en - US; scale = 2.00; 828x1792; 165586599)',
     'Cookie': 'sessionid=55174935431:oUkeyOkjSTHOai:11:AYcCQRGAQW0_XSTcjaz7XHUgRoXj6weyCzi_8MCzew'

1a1ac1c268a77478eb0ed43ae0c295a06c786875
shivam17acelucid <shivam.rawat@acelucid.com>
2023-02-02 11:21:37 +0530

    fetch apis

--- testing_backend/Controllers/Sorting/index.js
+++ testing_backend/Controllers/Sorting/index.js
@@ -17,7 +17,7 @@ exports.getProfileOfInfluencer = (req, res) => {
     let { inputField } = req.query;
 
     InfluencersData.findOne({ username: inputField })
-        .select({ username: 1, _id: 0, edge_followed_by: 1, city_name: 1, category_enum: 1, full_name: 1, profile_pic_url_hd: 1, edge_felix_video_timeline: 1, edge_owner_to_timeline_media: 1, })
+        .select({ username: 1, _id: 0, edge_followed_by: 1, city_name: 1, category_enum: 1, full_name: 1, profile_pic_url_hd: 1, edge_felix_video_timeline: 1, edge_owner_to_timeline_media: 1, final_category: 1, final_city: 1 })
         .then((data) => {
             res.json([data])
         })

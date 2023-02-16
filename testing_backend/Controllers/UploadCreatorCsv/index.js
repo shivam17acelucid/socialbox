b6bfaeaac1d6b76a9ddde3c77d0f2a55c52fef14
shivam17acelucid <shivam.rawat@acelucid.com>
2023-02-16 20:20:35 +0530

    chart

--- testing_backend/Controllers/UploadCreatorCsv/index.js
+++ testing_backend/Controllers/UploadCreatorCsv/index.js
@@ -43,7 +43,7 @@ exports.fetchCsvUsernames = (req, res, next) => {
     UsernameCsv.find({ isFetched: false })
         .then((response) => {
             response.forEach((item, i) => {
-                if (i < 50) {
+                if (i < 500) {
                     const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${item.username}`;
                     fetch(url,
                         {
